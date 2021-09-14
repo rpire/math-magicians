67a10dc65edc9f30d8ba597fbd56da5041c5d796
rpire <rubenpire7@gmail.com>
2021-09-14 17:30:43 -0400

    Apply fix for calculator.js Linter errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,29 +1,29 @@
-import React from "react";
+import React from 'react';
 
-class Calculator extends React.Component {
+class Calculator extends React.PureComponent {
   render() {
     return (
-      <div className='calculator'>
-        <input type='text' id='entry' className='calc' name='entry' placeholder='0' />
-        <div className='calc btn'>AC</div>
-        <div className='calc btn'>+/-</div>
-        <div className='calc btn'>%</div>
-        <div className='calc btn orange'>/</div>
-        <div className='calc btn'>7</div>
-        <div className='calc btn'>8</div>
-        <div className='calc btn'>9</div>
-        <div className='calc btn orange'>*</div>
-        <div className='calc btn'>4</div>
-        <div className='calc btn'>5</div>
-        <div className='calc btn'>6</div>
-        <div className='calc btn orange'>-</div>
-        <div className='calc btn'>1</div>
-        <div className='calc btn'>2</div>
-        <div className='calc btn'>3</div>
-        <div className='calc btn orange'>+</div>
-        <div id='zero' className='calc btn'>0</div>
-        <div className='calc btn'>.</div>
-        <div className='calc btn orange'>=</div>
+      <div className="calculator">
+        <input type="text" id="entry" className="calc" name="entry" placeholder="0" />
+        <div className="calc btn">AC</div>
+        <div className="calc btn">+/-</div>
+        <div className="calc btn">%</div>
+        <div className="calc btn orange">/</div>
+        <div className="calc btn">7</div>
+        <div className="calc btn">8</div>
+        <div className="calc btn">9</div>
+        <div className="calc btn orange">*</div>
+        <div className="calc btn">4</div>
+        <div className="calc btn">5</div>
+        <div className="calc btn">6</div>
+        <div className="calc btn orange">-</div>
+        <div className="calc btn">1</div>
+        <div className="calc btn">2</div>
+        <div className="calc btn">3</div>
+        <div className="calc btn orange">+</div>
+        <div id="zero" className="calc btn">0</div>
+        <div className="calc btn">.</div>
+        <div className="calc btn orange">=</div>
       </div>
     );
   }

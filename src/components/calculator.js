773a43bff1fac8fe62866d49d5488f0a3c4385cd
rpire <rubenpire7@gmail.com>
2021-09-15 14:19:48 -0400

    Apply fix for ESLint errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -11,32 +11,38 @@ class Calculator extends React.Component {
     }
     this.clickHandle = this.clickHandle.bind(this);
   }
-  clickHandle = async (event) => {
+
+  clickHandle = (event) => {
     this.setState(calculate(this.state, event.target.innerHTML));
   }
+
   render() {
     return (
       <div className="calculator">
-        <div id="entry" className="calc" name="entry" placeholder="0">{this.state.total}{this.state.operation}{this.state.next}</div>
-        <div className="calc btn" onClick={this.clickHandle}>AC</div>
-        <div className="calc btn" onClick={this.clickHandle}>+/-</div>
-        <div className="calc btn" onClick={this.clickHandle}>%</div>
-        <div className="calc btn orange" onClick={this.clickHandle}>÷</div>
-        <div className="calc btn" onClick={this.clickHandle}>7</div>
-        <div className="calc btn" onClick={this.clickHandle}>8</div>
-        <div className="calc btn" onClick={this.clickHandle}>9</div>
-        <div className="calc btn orange" onClick={this.clickHandle}>x</div>
-        <div className="calc btn" onClick={this.clickHandle}>4</div>
-        <div className="calc btn" onClick={this.clickHandle}>5</div>
-        <div className="calc btn" onClick={this.clickHandle}>6</div>
-        <div className="calc btn orange" onClick={this.clickHandle}>-</div>
-        <div className="calc btn" onClick={this.clickHandle}>1</div>
-        <div className="calc btn" onClick={this.clickHandle}>2</div>
-        <div className="calc btn" onClick={this.clickHandle}>3</div>
-        <div className="calc btn orange" onClick={this.clickHandle}>+</div>
-        <div id="zero" className="calc btn" onClick={this.clickHandle}>0</div>
-        <div className="calc btn" onClick={this.clickHandle}>.</div>
-        <div className="calc btn orange" onClick={this.clickHandle}>=</div>
+        <div id="entry" className="calc" name="entry" placeholder="0">
+          {this.state.total}
+          {this.state.operation}
+          {this.state.next}
+        </div>
+        <button className="calc btn" onClick={this.clickHandle}>AC</button>
+        <button className="calc btn" onClick={this.clickHandle}>+/-</button>
+        <button className="calc btn" onClick={this.clickHandle}>%</button>
+        <button className="calc btn orange" onClick={this.clickHandle}>÷</button>
+        <button className="calc btn" onClick={this.clickHandle}>7</button>
+        <button className="calc btn" onClick={this.clickHandle}>8</button>
+        <button className="calc btn" onClick={this.clickHandle}>9</button>
+        <button className="calc btn orange" onClick={this.clickHandle}>x</button>
+        <button className="calc btn" onClick={this.clickHandle}>4</button>
+        <button className="calc btn" onClick={this.clickHandle}>5</button>
+        <button className="calc btn" onClick={this.clickHandle}>6</button>
+        <button className="calc btn orange" onClick={this.clickHandle}>-</button>
+        <button className="calc btn" onClick={this.clickHandle}>1</button>
+        <button className="calc btn" onClick={this.clickHandle}>2</button>
+        <button className="calc btn" onClick={this.clickHandle}>3</button>
+        <button className="calc btn orange" onClick={this.clickHandle}>+</button>
+        <button id="zero" className="calc btn" onClick={this.clickHandle}>0</button>
+        <button className="calc btn" onClick={this.clickHandle}>.</button>
+        <button className="calc btn orange" onClick={this.clickHandle}>=</button>
       </div>
     );
   }

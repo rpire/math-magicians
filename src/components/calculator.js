f64f3507e27b0d8fe98952180c22c879860f277e
rpire <rubenpire7@gmail.com>
2021-09-15 13:40:42 -0400

    Import calculate to calculator.js and add the clickHandle function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,29 +1,42 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.PureComponent {
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null
+    }
+    this.clickHandle = this.clickHandle.bind(this);
+  }
+  clickHandle = async (event) => {
+    this.setState(calculate(this.state, event.target.innerHTML));
+  }
   render() {
     return (
       <div className="calculator">
-        <input type="text" id="entry" className="calc" name="entry" placeholder="0" />
-        <div className="calc btn">AC</div>
-        <div className="calc btn">+/-</div>
-        <div className="calc btn">%</div>
-        <div className="calc btn orange">/</div>
-        <div className="calc btn">7</div>
-        <div className="calc btn">8</div>
-        <div className="calc btn">9</div>
-        <div className="calc btn orange">*</div>
-        <div className="calc btn">4</div>
-        <div className="calc btn">5</div>
-        <div className="calc btn">6</div>
-        <div className="calc btn orange">-</div>
-        <div className="calc btn">1</div>
-        <div className="calc btn">2</div>
-        <div className="calc btn">3</div>
-        <div className="calc btn orange">+</div>
-        <div id="zero" className="calc btn">0</div>
-        <div className="calc btn">.</div>
-        <div className="calc btn orange">=</div>
+        <div id="entry" className="calc" name="entry" placeholder="0">{this.state.total}{this.state.operation}{this.state.next}</div>
+        <div className="calc btn" onClick={this.clickHandle}>AC</div>
+        <div className="calc btn" onClick={this.clickHandle}>+/-</div>
+        <div className="calc btn" onClick={this.clickHandle}>%</div>
+        <div className="calc btn orange" onClick={this.clickHandle}>÷</div>
+        <div className="calc btn" onClick={this.clickHandle}>7</div>
+        <div className="calc btn" onClick={this.clickHandle}>8</div>
+        <div className="calc btn" onClick={this.clickHandle}>9</div>
+        <div className="calc btn orange" onClick={this.clickHandle}>x</div>
+        <div className="calc btn" onClick={this.clickHandle}>4</div>
+        <div className="calc btn" onClick={this.clickHandle}>5</div>
+        <div className="calc btn" onClick={this.clickHandle}>6</div>
+        <div className="calc btn orange" onClick={this.clickHandle}>-</div>
+        <div className="calc btn" onClick={this.clickHandle}>1</div>
+        <div className="calc btn" onClick={this.clickHandle}>2</div>
+        <div className="calc btn" onClick={this.clickHandle}>3</div>
+        <div className="calc btn orange" onClick={this.clickHandle}>+</div>
+        <div id="zero" className="calc btn" onClick={this.clickHandle}>0</div>
+        <div className="calc btn" onClick={this.clickHandle}>.</div>
+        <div className="calc btn orange" onClick={this.clickHandle}>=</div>
       </div>
     );
   }

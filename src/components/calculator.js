91490824c8a1f423143fab860562e656b4e5d353
rpire <rubenpire7@gmail.com>
2021-09-15 14:38:00 -0400

    Apply second fix for Linter errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,42 +7,44 @@ class Calculator extends React.Component {
     this.state = {
       total: '0',
       next: null,
-      operation: null
-    }
+      operation: null,
+    };
     this.clickHandle = this.clickHandle.bind(this);
   }
 
   clickHandle = (event) => {
-    this.setState(calculate(this.state, event.target.innerHTML));
+    this.setState((prevState) => {
+      return (calculate(prevState, event.target.innerHTML));
+    });
   }
 
   render() {
     return (
       <div className="calculator">
         <div id="entry" className="calc" name="entry" placeholder="0">
-          {this.state.total}
-          {this.state.operation}
-          {this.state.next}
+          <span>{this.state.total}</span>
+          <span>{this.state.operation}</span>
+          <span>{this.state.next}</span>
         </div>
-        <button className="calc btn" onClick={this.clickHandle}>AC</button>
-        <button className="calc btn" onClick={this.clickHandle}>+/-</button>
-        <button className="calc btn" onClick={this.clickHandle}>%</button>
-        <button className="calc btn orange" onClick={this.clickHandle}>÷</button>
-        <button className="calc btn" onClick={this.clickHandle}>7</button>
-        <button className="calc btn" onClick={this.clickHandle}>8</button>
-        <button className="calc btn" onClick={this.clickHandle}>9</button>
-        <button className="calc btn orange" onClick={this.clickHandle}>x</button>
-        <button className="calc btn" onClick={this.clickHandle}>4</button>
-        <button className="calc btn" onClick={this.clickHandle}>5</button>
-        <button className="calc btn" onClick={this.clickHandle}>6</button>
-        <button className="calc btn orange" onClick={this.clickHandle}>-</button>
-        <button className="calc btn" onClick={this.clickHandle}>1</button>
-        <button className="calc btn" onClick={this.clickHandle}>2</button>
-        <button className="calc btn" onClick={this.clickHandle}>3</button>
-        <button className="calc btn orange" onClick={this.clickHandle}>+</button>
-        <button id="zero" className="calc btn" onClick={this.clickHandle}>0</button>
-        <button className="calc btn" onClick={this.clickHandle}>.</button>
-        <button className="calc btn orange" onClick={this.clickHandle}>=</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>AC</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>+/-</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>%</button>
+        <button type="button" className="calc btn orange" onClick={this.clickHandle}>÷</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>7</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>8</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>9</button>
+        <button type="button" className="calc btn orange" onClick={this.clickHandle}>x</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>4</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>5</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>6</button>
+        <button type="button" className="calc btn orange" onClick={this.clickHandle}>-</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>1</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>2</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>3</button>
+        <button type="button" className="calc btn orange" onClick={this.clickHandle}>+</button>
+        <button type="button" id="zero" className="calc btn" onClick={this.clickHandle}>0</button>
+        <button type="button" className="calc btn" onClick={this.clickHandle}>.</button>
+        <button type="button" className="calc btn orange" onClick={this.clickHandle}>=</button>
       </div>
     );
   }

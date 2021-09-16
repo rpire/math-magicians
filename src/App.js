4be1bddabef51ac689410731aa2bdd7340a2f0a0
rpire <rubenpire7@gmail.com>
2021-09-16 08:42:33 -0400

    Refactor App.js to implement hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from './logic/calculate';
 import Calculator from './components/calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: null,
-      operation: null,
-    };
-    this.changeHandle = this.changeHandle.bind(this);
+function App() {
+  const [data, setData] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
+  const changeHandle = (event) => {
+    setData(calculate(data, event.target.innerHTML));
   }
-
-  changeHandle = (event) => {
-    this.setState((prevState) => calculate(prevState, event.target.innerHTML));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    const obj = {
-      total,
-      next,
-      operation,
-    };
-
-    return (
-      <div className="container">
-        <h1>The Calculator Web App</h1>
-        <Calculator
-          data={obj}
-          onDataChange={this.changeHandle}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <h1>The Calculator Web App</h1>
+      <Calculator
+        data={data}
+        onDataChange={changeHandle}
+      />
+    </div>
+  );
 }
 
 export default App;

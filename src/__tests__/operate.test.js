eb983b319f231e86cd003289680ce98e4bc4944c
ridaarif98 <ridaarif16@gmail.com>
2021-09-22 12:28:22 +0200

    Intialize test for multiply, divide, modulus and Quote.js

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -9,3 +9,32 @@ describe('Sum', () => {
     expect(operate('543', '0', '+')).toBe('543');
   })
 });
+
+describe('Multiply', () => {
+  test('5 × 3 = 15', () => {
+    expect(operate('5','3','×')).toBe('15');
+  });
+  test('5 × 0 = 0', () => {
+    expect(operate('5','0','×')).toBe('0');
+  });
+});
+
+describe('Divide', () => {
+  test('15 ÷ 3 = 5', () => {
+    expect(operate('15','3','÷')).toBe('5');
+  });
+  test('5 ÷ 0 = 0', () => {
+    expect(operate('5','0','÷')).toBe('Can\'t divide by 0.');
+  });
+});
+
+describe('Modulus', () => {
+  test('15 % 3 = 0', () => {
+    expect(operate('15','3','%')).toBe('0');
+  });
+  test('5 % 0 = 0', () => {
+    expect(operate('5','0','%')).toBe('Can\'t divide by 0.');
+  });
+});
+
+

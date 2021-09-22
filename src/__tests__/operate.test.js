f91e951011d39c8fc12e1d0552eb87e06d359025
rpire <rubenpire7@gmail.com>
2021-09-22 06:29:53 -0400

    Add tests for Plus and Minus

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -1,11 +1,29 @@
 import operate from '../logic/operate';
 
-describe('Sum', () => {
-  test('1 + 1 = 2', () => {
+describe('Plus', () => {
+  test('Adding one plus one', () => {
     expect(operate('1', '1', '+')).toBe('2');
   });
 
-  test('Sum with zero', () => {
+  test('Adding zero', () => {
     expect(operate('543', '0', '+')).toBe('543');
-  })
+  });
+
+  test('Adding a negative number', () => {
+    expect(operate('17', '-5', '+')).toBe('12');
+  });
 });
+
+describe('Minus', () => {
+  test('Subtracting five from 7', () => {
+    expect(operate('7', '5', '-')).toBe('2');
+  });
+
+  test('Subtracting 7 from five', () => {
+    expect(operate('5', '7', '-')).toBe('-2');
+  })
+
+  test('Subtracting two negative numbers', () => {
+    expect(operate('-17', '-30', '-')).toBe('13');
+  })
+})
